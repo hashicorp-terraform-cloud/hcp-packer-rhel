a4b695c7908f800971132f4bafa558026ad9b2f4
Ben Holmes <ben.holmes@hashicorp.com>
2023-05-02 12:33:43 +0100

    debug

--- rhel9/main.tf
+++ rhel9/main.tf
@@ -5,19 +5,7 @@ resource "null_resource" "packer" {
 
   provisioner "local-exec" {
     command = <<EOF
-RED='\033[0;31m' # Red Text
-GREEN='\033[0;32m' # Green Text
-BLUE='\033[0;34m' # Blue Text
-NC='\033[0m' # No Color
-
-packer build .
-
-if [ $? -eq 0 ]; then
-  printf "\n $GREEN Packer Succeeded $NC \n"
-else
-  printf "\n $RED Packer Failed $NC \n" >&2
-  exit 1
-fi
+export
 EOF
   }
 }
